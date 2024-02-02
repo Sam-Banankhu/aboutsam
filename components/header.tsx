04154a9ae16b5c81e3d03c386f76965d9f40f5e5
Sam-Banankhu <sambanankhumhango@gmail.com>
2024-02-03 00:14:10 +0200

    fixed header motion failure

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { motion } from "framer-motion";
 
@@ -6,8 +8,8 @@ export default function header() {
     <header className="fixed left-0 top-0 z-[999] w-full sm:w-[initial]">
       <motion.div
         className="fixed left-0 top-0 flex h-[4.5rem] w-full !translate-x-0 rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 px-4 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-gray-950 dark:bg-opacity-75 dark:shadow-lg dark:shadow-black/[0.03] sm:left-1/2 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:!-translate-x-1/2 sm:rounded-full"
-        // initial={{ y: -100, x: "-50%", opacity: 0 }}
-        // animate={{ y: 0, x: "-50%", opacity: 1 }}
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
     </header>
   );

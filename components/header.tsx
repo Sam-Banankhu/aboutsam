7d264d3753c7005f6452dd86b1b25c2082d36d8a
Sam-Banankhu <sambanankhumhango@gmail.com>
2024-02-03 00:52:19 +0200

    added nav links and styled

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useTheme } from "@/context/theme-context";
+import { links } from "@/lib/data";
+// import { cn } from "@/lib/utils";
 
 export default function header() {
   return (
@@ -11,6 +16,27 @@ export default function header() {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
+
+      {/* Navigations */}
+      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        {/* Navigation Links */}
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {/* <AnimatePresence> */}
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center"
+              key={link.hash}
+            >
+              <Link
+                className="flex w-full justify-center px-3 py-3 hover:text-gray-950 transition"
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }

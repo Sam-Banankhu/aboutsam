c735f8da204cde4d5f6ed224efdab715034d064c
Sam-Banankhu <sambanankhumhango@gmail.com>
2024-02-03 22:46:49 +0200

    the projects been fixed

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,36 @@
 import React from "react";
-
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
 export default function Projects() {
-  return <div></div>;
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div className="m-auto mt-8">
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+}: (typeof projectsData)[number]) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }

6be0813528d5d49c657dc19d2259a70d43638c48
Sam-Banankhu <sambanankhumhango@gmail.com>
2024-02-03 09:52:43 +0200

    added motion to contact section

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,6 +5,7 @@ import { FaGithubSquare } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
 import { motion } from "framer-motion";
 import React from "react";
+import Link from "next/link";
 
 export default function Intro() {
   return (
@@ -86,6 +87,54 @@ export default function Intro() {
         </span>
         In web, my focus is <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      {/* Contact Section */}
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
+      >
+        <Link
+          href={"#contact"}
+          className="group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none transition hover:scale-110 hover:bg-gray-950 focus:scale-110 focus:bg-gray-950 active:scale-105"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 transition group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          href="/CV.pdf"
+          download
+          className="group flex items-center gap-2 rounded-full border border-black/10 bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105 dark:bg-white/10"
+        >
+          Download CV
+          <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
+        </a>
+
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          className="flex items-center rounded-full border border-black/10 bg-white p-4 text-gray-700 outline-none transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] focus:text-gray-950 active:scale-105 dark:bg-white/10 dark:text-white/60"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com"
+          target="_blank"
+          className="flex items-center rounded-full border border-black/10 bg-white p-4 text-[1.35rem] text-gray-700 outline-none transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] focus:text-gray-950 active:scale-105 dark:bg-white/10 dark:text-white/60"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }

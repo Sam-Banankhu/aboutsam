3c57001e429282deb0005ada2a2789a487111e33
Sam-Banankhu <sambanankhumhango@gmail.com>
2024-02-03 08:22:25 +0200

    added profile photo

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,12 +7,13 @@ export default function Intro() {
       <div className="flex items-center justify-center">
         <div>
           <Image
-            src="/public/PXL_20221205_070951353.jpg"
+            src="https://media.licdn.com/dms/image/D4D03AQFlsU8IZ_99hA/profile-displayphoto-shrink_200_200/0/1670307795304?e=1712188800&v=beta&t=NoJhdHQsuaqaRiZpUdEv2kFx6ICtPxlWYnuxP1utDC4"
             alt="Profile"
             width={192}
             height={192}
             quality={100}
             priority={true}
+            className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
           />
         </div>
       </div>

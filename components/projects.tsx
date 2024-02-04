63f1c505ca20b882a21d4db437a17cd7ade5c340
Sam Banankhu <sambanankhumhango@gmail.com>
2024-02-04 20:24:36 +0200

    added transitions to projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -26,7 +26,8 @@ function Project({
   return (
     <section className="group bg-gray-100 max-w-[42rem] border 
     border-black/5 
-    overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8">
+    overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8
+    hover:bg-gray-200 transition">
       
       {/* for title and description */}
       <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
